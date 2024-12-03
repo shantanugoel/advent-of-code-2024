b0680f82d11fb9ac15bf6359084bb9668a522b99
Shantanu Goel <shantanu.goel@gmail.com>
2024-12-03 11:06:22 +0530

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -22,6 +22,38 @@ pub fn part1() -> Answer {
     sum.into()
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum MulState {
+    Enabled,
+    Disabled,
+}
+
+fn get_input_part2() -> Vec<(i64, i64)> {
+    let lines = utils::read_lines("./inputs/day3");
+    let re = Regex::new(r"(do)\(\)|(don)\'t\(\)|mul\((\d+),(\d+)\)").unwrap();
+    let mut input: Vec<(i64, i64)> = Vec::new();
+    let mut state = MulState::Enabled;
+    for line in lines.iter() {
+        for capture in re.captures_iter(line) {
+            if capture.get(1).is_some() {
+                state = MulState::Enabled;
+            } else if capture.get(2).is_some() {
+                state = MulState::Disabled;
+            } else if state == MulState::Enabled {
+                let a = capture.get(3).unwrap().as_str();
+                let b = capture.get(4).unwrap().as_str();
+                input.push((a.parse().unwrap(), b.parse().unwrap()));
+            }
+        }
+    }
+    input
+}
+
 pub fn part2() -> Answer {
-    0.into()
+    let input = get_input_part2();
+    let mut sum: i64 = 0;
+    for (a, b) in input.iter() {
+        sum += a * b;
+    }
+    sum.into()
 }
