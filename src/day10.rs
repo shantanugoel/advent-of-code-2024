7512c718be6c34b8157172a4dc189b9553d6640a
Shantanu Goel <shantanu.goel@gmail.com>
2024-12-09 18:08:02 +0530

    Day 10 prep

---
+++ src/day10.rs
@@ -0,0 +1,24 @@
+use crate::utils::Answer;
+
+pub fn part1(_: &str) -> Answer {
+    todo!();
+}
+
+pub fn part2(_: &str) -> Answer {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("./inputs/day10_sample"), 0.into());
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("./inputs/day10_sample"), 0.into());
+    }
+}
