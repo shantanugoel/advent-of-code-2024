45cca67c934fb3428d0f9bb4020bd305ce6dfd56
Shantanu Goel <shantanu.goel@gmail.com>
2024-12-11 17:08:20 +0530

    Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::utils::{self, Answer};
 
 fn get_input(input: &str) -> Vec<u64> {
@@ -5,28 +7,48 @@ fn get_input(input: &str) -> Vec<u64> {
     lines[0].split(" ").map(|x| x.parse().unwrap()).collect()
 }
 
-pub fn parts(input: &str, num_blinks: u32) -> Answer {
-    let mut stones = get_input(input);
+fn blink(stone: &u64, num_blinks: u32, memory: &mut HashMap<(u64, u32), u64>) -> u64 {
+    let mut new_stones = Vec::new();
+    let mut count = 0;
 
-    for _ in 0..num_blinks {
-        let mut new_stones = Vec::new();
-        for stone in stones.iter() {
-            let stone_string = stone.to_string();
-            if *stone == 0 {
-                new_stones.push(1);
-            } else if stone_string.len() % 2 == 0 {
-                let left = &stone_string[0..stone_string.len() / 2];
-                let right = &stone_string[stone_string.len() / 2..stone_string.len()];
-                new_stones.push(left.parse().unwrap());
-                new_stones.push(right.parse().unwrap());
-            } else {
-                new_stones.push(stone * 2024);
-            }
-        }
-        stones = new_stones;
+    if memory.contains_key(&(*stone, num_blinks)) {
+        return memory[&(*stone, num_blinks)];
+    }
+    if *stone == 0 {
+        new_stones.push(1);
+    } else if stone.to_string().len() % 2 == 0 {
+        let left = &stone.to_string()[0..stone.to_string().len() / 2];
+        let right = &stone.to_string()[stone.to_string().len() / 2..stone.to_string().len()];
+        new_stones.push(left.parse().unwrap());
+        new_stones.push(right.parse().unwrap());
+    } else {
+        new_stones.push(stone * 2024);
     }
 
-    stones.len().into()
+    memory.insert((*stone, 1), new_stones.len() as u64);
+
+    if num_blinks == 1 {
+        count = new_stones.len() as u64;
+    } else {
+        for new_stone in new_stones.iter() {
+            let temp = blink(new_stone, num_blinks - 1, memory);
+            memory.insert((*new_stone, num_blinks - 1), temp);
+            count += temp;
+        }
+    }
+
+    count
+}
+
+pub fn parts(input: &str, num_blinks: u32) -> Answer {
+    let stones = get_input(input);
+    let mut count = 0;
+
+    for stone in stones.iter() {
+        count += blink(stone, num_blinks, &mut HashMap::new());
+    }
+
+    count.into()
 }
 
 #[cfg(test)]
@@ -35,11 +57,6 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(parts("./inputs/day11_sample2", 25), 55312i32.into());
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(parts("./inputs/day11_sample", 75), 2713310158u64.into());
+        assert_eq!(parts("./inputs/day11_sample2", 25), 55312u64.into());
     }
 }
